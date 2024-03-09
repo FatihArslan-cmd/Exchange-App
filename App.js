9b032f6306dc863a9c5a509198d3458427d61a56
FatihArslan-cmd <122049858+FatihArslan-cmd@users.noreply.github.com>
2024-03-09 23:13:19 +0300

    The tabBar error has been handled

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image,View } from 'react-native';
+import { Image, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import CurrencyScreen from './CurrencyScreen';
@@ -8,13 +8,10 @@ import SettingsScreen from './SettingsScreen';
 import NewsScreen from './NewsScreen';
 import CoinsScreen from './coinsScreen';
 import FavoriesScreen from './FavoriesScreen';
+
 const Tab = createBottomTabNavigator();
 
-
-
-
 export default function App() {
-  
   return (
     <NavigationContainer>
       <Tab.Navigator
@@ -30,8 +27,7 @@ export default function App() {
               iconName = focused
                 ? require('./assets/icons8-settings-50 (1).png')
                 : require('./assets/icons8-settings-50.png');
-            }
-            else if (route.name === 'News') {
+            } else if (route.name === 'News') {
               iconName = focused
                 ? require('./assets/icons8-news-50 (3).png')
                 : require('./assets/icons8-news-50 (2).png');
@@ -39,52 +35,50 @@ export default function App() {
             return <Image source={iconName} style={{ width: 20, height: 20 }} />;
           },
         })}
-        tabBarOptions={{
-          activeTintColor: 'black',
-          inactiveTintColor: 'gray',
-        }}
       >
         <Tab.Screen
           name="Home"
           component={HomeScreen}
-          options={{ headerShown: false }}
+          options={{ headerShown: false, tabBarActiveTintColor: 'black', tabBarInactiveTintColor: 'gray', tabBarStyle: { display: 'flex' } }}
         />
         <Tab.Screen
           name="CurrencyScreen"
           component={CurrencyScreen}
-          options={{ headerShown: false , tabBarButton: () => (
-            <View style={{width:0, height:0}}></View>),
-          tabBarVisible:false}}
-      
+          options={{
+            headerShown: false,
+            tabBarButton: () => <View style={{ width: 0, height: 0 }} />,
+            tabBarVisible: false,
+          }}
         />
         <Tab.Screen
           name="News"
           component={NewsScreen}
-          options={{ headerShown: false  }}
+          options={{ headerShown: false, tabBarActiveTintColor: 'black', tabBarInactiveTintColor: 'gray', tabBarStyle: { display: 'flex' } }}
         />
         <Tab.Screen
           name="CoinsScreen"
           component={CoinsScreen}
-          options={{ headerShown: false , tabBarButton: () => (
-            <View style={{width:0, height:0}}></View>
-        ),
-              tabBarVisible:false}}
+          options={{
+            headerShown: false,
+            tabBarButton: () => <View style={{ width: 0, height: 0 }} />,
+            tabBarVisible: false,
+          }}
         />
         <Tab.Screen
           name="FavoriesScreen"
           component={FavoriesScreen}
-          options={{ headerShown: false , tabBarButton: () => (
-            <View style={{width:0, height:0}}></View>
-        ),
-        tabBarVisible:false}}
+          options={{
+            headerShown: false,
+            tabBarButton: () => <View style={{ width: 0, height: 0 }} />,
+            tabBarVisible: false,
+          }}
         />
         <Tab.Screen
           name="Settings"
           component={SettingsScreen}
-          options={{ headerShown: false  }}
+          options={{ headerShown: false, tabBarActiveTintColor: 'black', tabBarInactiveTintColor: 'gray', tabBarStyle: { display: 'flex' } }}
         />
       </Tab.Navigator>
-      
     </NavigationContainer>
   );
 }
